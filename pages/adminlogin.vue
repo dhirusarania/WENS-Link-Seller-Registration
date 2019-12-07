919d5376cd8bcb7e5041e269d68d4d358e9d06fe
dhirusarania <dhirusarania@gmail.com>
2019-12-07 14:40:45 +0530

    home

---
+++ pages/adminlogin.vue
@@ -0,0 +1,20 @@
+<template> </template>
+<script>
+export default {
+  mounted() {
+    console.log(this.$route.query.token);
+
+    this.$cookies.removeAll();
+
+    this.$cookies.set("access_token", this.$route.query.token, {
+      path: "/",
+      // httpOnly : true,
+      // secure: true,
+      maxAge: 60 * 60 * 24 * 7
+    });
+
+    this.$store.dispatch('getStep')
+
+  }
+};
+</script>
